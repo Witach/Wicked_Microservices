272550ced313c6e7fa69428a64c56cbb829fce7b
Witach <witach98@gmail.com>
2023-03-09 23:18:29 +0100

    keycloak exported config files fix

--- profile-app/src/main/kotlin/com/example/configuration/event-listeners.kt
+++ profile-app/src/main/kotlin/com/example/configuration/event-listeners.kt
@@ -5,24 +5,32 @@ import com.example.servicechassis.ImperativeSessionStorage
 import com.example.servicechassis.toUUID
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.kafka.annotation.KafkaListener
+import java.util.*
 
 
 class EventListeners(val profileService: ProfileService, val objectMapper: ObjectMapper, val imperativeSessionStorage: ImperativeSessionStorage) {
 
     @KafkaListener(topics = ["profile-removed-event"])
     fun `profile-removed-event`(event: String) {
-
-        val group = (objectMapper.readTree(event)["group"].toString().replace("\"", "")).toUUID()
-        val profile = (objectMapper.readTree(event)["profile"].toString().replace("\"", "")).toUUID()
+        val group = readGroup(event)
+        val profile = readProfile(event)
         imperativeSessionStorage.userId = profile
         profileService.removeFromGroup(profile, group)
     }
 
     @KafkaListener(topics = ["profile-added-event"])
     fun `profile-added-event`(event: String) {
-        val group = (objectMapper.readTree(event)["group"].toString().replace("\"", "")).toUUID()
-        val profile = (objectMapper.readTree(event)["profile"].toString().replace("\"", "")).toUUID()
+        val group = readGroup(event)
+        val profile = readProfile(event)
         imperativeSessionStorage.userId = profile
         profileService.addToGroup(profile, group)
+    }
+
+    fun readGroup(event: String): UUID {
+        return (objectMapper.readTree(event)["group"].toString().replace("\"", "")).toUUID()
+    }
+
+    fun readProfile(event: String): UUID {
+        return (objectMapper.readTree(event)["profile"].toString().replace("\"", "")).toUUID()
     }
 }
